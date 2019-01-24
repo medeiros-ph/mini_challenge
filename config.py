8248532c7300619171609a89e42137dfe02982d9
Raphael Medeiros <medeiros.ph@gmail.com>
2019-01-24 21:59:09 +0000

    add config

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+
+    def __init__(self):
+
+        self.arquivo_csv='/home/medeiros/mini_challenge/dataset_images_minitest.csv'
+        self.imagePath='/home/medeiros/mini_challenge/data_folder'
+
--- mini_challenge_cnn.py
+++ mini_challenge_cnn.py
@@ -3,7 +3,9 @@
 # Part 1 - Building the CNN
 # Importing the Keras libraries and packages
 
-import sys, os
+import sys
+import os
+import os.path
 from keras import backend as K
 
 from keras.models import Sequential
@@ -16,16 +18,35 @@ from keras.layers import Dense
 from keras.layers.core import Dropout
 
 from subprocess import check_output
+import numpy as np
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
+from config import Config
+from sklearn.model_selection import train_test_split
 from PIL import Image
 
+config = Config()
+
+dado = pd.read_csv(config.arquivo_csv).values
+
+imagem = dado[0][0]
+categoria = dado[0][1]
+
+X = []
+y = []
+
+quantity= sum(os.path.isfile(os.path.join(config.imagePath, f)) for f in os.listdir(config.imagePath))
+
+for item in range(quantity):
+
+    X.append(dado[item][0])
+    y.append(dado[item][1])
 
 
-train = pd.read_csv("input/dataset_images_minitest (train).csv").values
-test  = pd.read_csv("input/dataset_images_minitest (test).csv").values
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.33, random_state=42)
 
-print (train)
+print()
+
+
 
 # Reshape and normalize training data
 
