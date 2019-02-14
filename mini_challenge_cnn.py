b113984b864dd235dace91518684d6cced2e2870
“Raphael <medeiros.ph@gmail.com>
2019-02-14 20:09:54 -0200

    “PontoPySemErr”

--- mini_challenge_cnn.py
+++ mini_challenge_cnn.py
@@ -13,12 +13,14 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.layers.core import Dropout
+from keras.preprocessing.image import ImageDataGenerator
 
 from subprocess import check_output
 import numpy as np
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from config import Config
 from sklearn.model_selection import train_test_split
+
 from PIL import Image
 
 config = Config()
@@ -45,13 +47,25 @@ for item in range(quantity):
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.33, random_state=42)
 
 
-train_set = train_datagen.flow_from_directory('/home/medeiros/mini_challenge/data_folder',
+train_datagen = ImageDataGenerator(
+rescale=1./255,
+shear_range=0.2,
+zoom_range=0.2,
+horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+
+train_generator = train_datagen.flow_from_directory(directory=r"/home/medeiros/mini_challenge/data_folder",
                                              target_size = (256, 256),
                                              color_mode = 'rgb',
                                              classes = ['graduation', 'meeting', 'picnic'],
                                              batch_size = 32,
-                                             class_mode = 'categorical') #Recebe um diretório e caminha por ele
-                                                                        #com base nos parâmetros fornecidos
+                                             class_mode = 'categorical',
+                                             shuffle=True,
+                                             seed=42
+ ) #Recebe um diretório e caminha por ele
+   #com base nos parâmetros fornecidos
 
 
 test_set = test_datagen.flow_from_directory('/home/medeiros/mini_challenge/data_folder',
@@ -94,76 +108,5 @@ model.add(Dense(activation = 'relu',
 model.add(Dense(activation = 'softmax',
                units = 3))#FC3
 
-print()
 
 
-################################### VERSAO ANTERIOR #########################################
-
-#Data Preprocessing
-from config import Config
-
-#Importing Labraries
-import sys
-import os
-import os.path
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-
-# Importing the Keras libraries and packages
-from keras import backend as K
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.core import Activation
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
-from keras.layers.core import Dropout
-from subprocess import check_output
-
-
-from PIL import Image
-
-#Import the dataset
-config = Config()
-
-dado = pd.read_csv(config.arquivo_csv).values
-
-imagem = dado[0][0]
-categoria = dado[0][1]
-
-X = []
-y = []
-
-# ou X = dado.iloc[:,:1].values
-#    y = dado.iloc[:,:2].values
-
-quantity= sum(os.path.isfile(os.path.join(config.imagePath, f)) for f in os.listdir(config.imagePath))
-
-for item in range(quantity):
-
-    X.append(dado[item][0])
-    y.append(dado[item][1])
-
-
-X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.33, random_state=42)
-
-
-
-print()
-
-
-
-# Reshape and normalize training data
-
-
-# Reshape and normalize test data
-
-from sklearn import preprocessing
-lb = preprocessing.LabelBinarizer()
-
-# Part 1 - Building the CNN
